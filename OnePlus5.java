dba32f923279e5f23aa68c6b8541ce0cf1482880
sandeeptkr07 <145425816+sandeeptkr07@users.noreply.github.com>
2023-10-16 20:31:46 +0530

    Add files via upload

---
+++ OnePlus5.java
@@ -0,0 +1,45 @@
+package com.coreconcepts.interfacee;
+
+public class OnePlus5 implements Phone{
+
+	@Override
+	public String processor() {
+		// TODO Auto-generated method stub
+		return "SD835";
+	}
+
+	@Override
+	public String OS() {
+		// TODO Auto-generated method stub
+		return "Android";
+	}
+
+	@Override
+	public int spaceInGB() {
+		// TODO Auto-generated method stub
+		return 64;
+	}
+	
+	public static void main(String[] args) {
+		Phone oneplus = new OnePlus5();
+		System.out.println("OS: "+ oneplus.OS());
+		System.out.println("Processor: "+ oneplus.processor());
+		System.out.println("spaceinGB: "+ oneplus.spaceInGB());
+		
+		Phone ios = new Iphone8();
+		System.out.println("OS: "+ ios.OS());
+		System.out.println("Processor: "+ ios.processor());
+		System.out.println("spaceinGB: "+ ios.spaceInGB());
+		
+		Phone s1 = new Samsung();
+		System.out.println("OS: "+ s1.OS());
+		System.out.println("Processor: "+ s1.processor());
+		System.out.println("spaceinGB: "+ s1.spaceInGB());
+		
+		
+	}
+
+}
+
+
+
---
+++ Samsung.java
@@ -0,0 +1,24 @@
+package com.coreconcepts.interfacee;
+
+public class Samsung implements Phone {
+
+	@Override
+	public String processor() {
+		// TODO Auto-generated method stub
+		return "s15";
+	}
+
+	@Override
+	public String OS() {
+		// TODO Auto-generated method stub
+		return "android" ;
+	}
+
+	@Override
+	public int spaceInGB() {
+		// TODO Auto-generated method stub
+		return 128;
+	}
+	
+
+}
